9d52ece60a57570fdc2de880cdba11cce07df470
Tasper <tiffanyasper@gmail.com>
2025-08-13 13:46:18 -0700

    created files, created imagecard component

---
+++ 05-Vue_Expanded/assignment/lesson5-assignment/src/components/BreedsComp.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="container my-3">
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+      <div>
+        <ImageCardComp
+          imageSrc="images/chicken-thumbnail-350.jpg"
+          alt="chicken"
+          title="Chicken Breed"
+          cardText="Lorem ipsum dolor sit amet
+        consectetur, adipisicing elit. Dicta possimus, cumque consectetur eos ad
+        quis nostrum deleniti asperiores, pariatur dolorum reiciendis corporis
+        nobis architecto, quaerat molestias iste vero sit ut."
+          url="#"
+          linkText="Source Locally"
+        />
+      </div>
+    </div>
+  </section>
+</template>
+<script setup>
+import ImageCardComp from "./Image-CardComp.vue";
+</script>
+<style></style>
---
+++ 05-Vue_Expanded/assignment/lesson5-assignment/src/components/Image-CardComp.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="image-card">
+    <img class="card-img-top" :src="props.imageSrc" :alt="props.alt" />
+    <div class="card-body">
+      <h5 class="card-title">{{ props.title }}</h5>
+      <p class="card-text">{{ props.cardText }}</p>
+      <a :href="props.url" class="btn btn-success">{{ props.linkText }}</a>
+    </div>
+  </div>
+</template>
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  imageSrc: {
+    type: String,
+    required: true,
+  },
+  alt: {
+    type: String,
+    default: "image",
+  },
+  title: String,
+  cardText: String,
+  url: String,
+  linkText: String,
+});
+</script>
+<style scoped></style>
